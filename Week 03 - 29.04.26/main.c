d9c620f0dc4f4cd3dd6c398f2e9374c8e379901d
bsharabi <bsharabi95@gmail.com>
2026-04-29 11:04:26 +0300

    up

--- Week 03 - 29.04.26/main.c
+++ Week 03 - 29.04.26/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
 
+    atoi(argv[2]);
+    printf("argc -> (%d)\n", argc);
+    for (int i = 0; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+
     printf("Hello\n");
-
-
 
     /* code */
     return 0;
